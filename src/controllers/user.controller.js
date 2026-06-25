42af9fab85ffd098f28f3aa73a0e4e95f8495786
Harsh-iconic <harshsoalnki4@gmail.com>
2026-06-25 23:36:07 +0530

    Add channel profile retrieval with subscriber details in getUserChannelProfile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -265,6 +265,65 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
         throw new ApiError(400, "username is missing")
     }
 
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                subscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {
+                            $in: [req.user?._id, "$subscribers.subscriber"]
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                email: 1,
+                subscribersCount: 1,
+                subscribedToCount: 1,
+                isSubscribed: 1
+            }
+        }
+
+    ])
+
+    if(!channel?.length) {
+        throw new ApiError(404, "Channel not found")
+    }
+    return res.status(200).json(new ApiResponse(200, channel[0], "Channel profile fetched successfully"))   
+
 })
 
 export {
